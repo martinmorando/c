a6525abf8a2197223d250389787752a9874da66c
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-06-07 18:25:26 -0300

    add 003-printf.c

---
+++ 003-printf.c
@@ -0,0 +1,24 @@
+/*
+    Printf
+    - %i: integer
+    - %f: float or double
+    - %c: char
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    // Integer
+    int age = 20;
+    printf("%i\n", age);
+
+    // Float or double
+    float price = 16.79;
+    printf("%f\n", price);
+
+    // Char
+    char letter = 'C';
+    printf("%c\n", letter);
+
+}
