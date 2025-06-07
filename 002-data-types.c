76fff36c032034d04721bfea146dbc2c41089cef
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-06-07 18:25:14 -0300

    add 002-data-types.c

---
+++ 002-data-types.c
@@ -0,0 +1,18 @@
+/*
+    Data types
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    int beers = 2;
+    float tony_stark_net_worth_usd = 80000000000;
+    double price = 16.79; // More precision than float
+    char letter = 'B';    // Single quotes
+
+    // Constant
+    const int total_coins = 21;
+    // total_coins = 22; This would throw an error
+
+}
