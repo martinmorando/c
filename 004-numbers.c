fea44cbef454fc1e16368df562a3761f449dba15
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-06-08 12:43:28 -0300

    add 004-numbers.c

---
+++ 004-numbers.c
@@ -0,0 +1,33 @@
+/*
+    Numbers
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    int a = 1;
+    int b = 2;
+
+
+    // [BASIC OPERATIONS]
+    a + b;          // 3
+    a - b;          // 1
+    a * b;          // 2
+    a / b;          // 2
+    a % b;          // 0
+
+
+    // [COMPOUND ASSIGNMENT OPERATORS]
+    a += 3;         // 4    
+    a -= 2;         // 2    
+    a *= 2;         // 4    
+    a /= 2;         // 2    
+    a %= 2;         // 0
+
+
+    // [INCREMENT & DECREMENT OPERATORS]
+    a++;            // 2
+    b--;            // 1
+
+}
