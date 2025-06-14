80f1a1a6f22b5eadcffdab6a3027b2236ed4270d
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-06-14 20:24:15 -0300

    add 'Replace' and 'Functions from <string.h>' sections in 008-strings.c

--- 008-strings.c
+++ 008-strings.c
@@ -3,15 +3,34 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
-    // Alternative A
+    // [CREATE]
+    // - Alternative A
     char messageA[] = "The internet and AI have made the formal educational system obsolete. The pessimistic teachers will have to work now.";
     printf("%s\n", messageA);
     
-    // Alternative B: notice the "null terminating character" (\0) 
+    // - Alternative B: notice the "null terminating character" (\0) 
     char messageB[] = {'K', 'e', 'e', 'p', ' ', 'g', 'o', 'i', 'n', 'g', '!', '\0'};
     printf("%s\n", messageB);
+
+    
+    // [REPLACE]
+    messageB[10] = '.';
+    printf("%s\n", messageB);
+
+
+    // [FUNCTIONS FROM <string.h>]
+    // - strlen(): returns number of characters in string; omits \0
+    int lengthB = strlen(messageB);
+    printf("%i\n", lengthB);
+
+    // - strcat(a, b): appends characters from 'b' to 'a'. IMPORTANT: 'a' must have enough space to prevent buffer overflow
+    char new_string_a[11] = "aaaaa"; // aaaaa (5) + bbbbb (5) + \0 (1)
+    char new_string_b[] = "bbbbb";
+    strcat(new_string_a, new_string_b);
+    printf("%s\n", new_string_a);
 
 }
