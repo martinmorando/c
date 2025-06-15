58650a14807b83a61fbc7937822f09fbba002157
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-06-15 14:36:05 -0300

    add 009-pointers.c

---
+++ 009-pointers.c
@@ -0,0 +1,13 @@
+/*
+    Pointers
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    int var = 1;    
+    int *pointer = &var;   // Pointer to the variable
+    printf("%p", pointer); // Address of the variable
+    
+}
