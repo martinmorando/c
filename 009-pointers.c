a58fb1102f85eea2bb5f40f0f2d103d94e514951
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-06-16 15:36:25 -0300

    add dereference operator, arrays and comments in 009-pointers.c

--- 009-pointers.c
+++ 009-pointers.c
@@ -1,13 +1,33 @@
 /*
     Pointers
+    - &: reference operator.
+    - *: left of the equal sign                      --> pointer declaration (defines a pointer variable).
+         right of the equal sign (or in expressions) --> dereference operator (accesses the value at the address).
+    - Addition and subtraction are the only operations allowed on pointers (not
+      between them), and they are used to navigate through memory addresses.
 */
 
 #include <stdio.h>
 
 int main() {
 
-    int var = 1;    
-    int *pointer = &var;   // Pointer to the variable
-    printf("%p", pointer); // Address of the variable
+    // [POINTERS]
+    int var = 1;
+    // Pointer to the variable. Contains the memory address of 'var'
+    int *pointer = &var;      // Also valid: int* pointer = &var;
+    printf("%p\n", pointer);
+
+    // Dereference the pointer to access its value
+    printf("%i\n", *pointer);
+
+
+    // [POINTERS & ARRAYS]
+    // Loop through an array using a pointer as arrays are contigous blocks of memory.
+    int arr[] = {3, 4, 5, 6, 7};
+    int* pointer_2 = &arr[0];
+    for(int i=0; i<sizeof(arr)/sizeof(int); i++) {
+        printf("%i ", *pointer_2);
+        pointer_2++;
+    } 
     
 }
